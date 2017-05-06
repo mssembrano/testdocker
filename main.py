7c4c5769e82d511f7c09f68b38377cad7e0e0e88
jonelmawirat <jonel.mawirat@bugomi.com>
2017-05-06 04:15:12 +0000

    Python Programming  Commit Conditional2 Ping

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os 
 
-response = os.system("ping -c 2 8.8.8.8")
+response1 = os.system("ping -c 1 8.8.8.8")
+response2 = os.system("ping -c 1 8.8.8.4")
+response3 = os.system("ping -c 1 192.168.1.1")
+response4 = os.system("ping -c 1 192.168.1.2")
 
 
-if response == 0:
-    print ("Active Account!")
-elif response ==256:
-    print("Account Deactivated")
-else:
-    print("Hu U")
+if response1 == 0:
+    print ("8.8.8.8 Reachable")
+elif response1 ==256:
+    print ("8.8.8.8 Unreachable")
+
+if response2 == 0:
+    print ("8.8.8.4 Reachable ")
+elif response2 ==256:
+    print ("8.8.8.4 Unreachable ")
+
+if response3 == 0:
+    print ("192.168.1.1 Reachable ")
+elif response3 ==256:
+    print ("192.168.1.1 Ureachable ")
+
+if response4 == 0:
+    print ("192.168.1.2 Reachable")
+elif response4 ==256:
+    print ("192.168.1.2 Ureachable ")
