48ea2b1acda2a149c43c09fe709681565d912211
jonelmawirat <jonel.mawirat@bugomi.com>
2017-05-06 05:17:45 +0000

    Python Programming  Commit My Logic2 short in Ping

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import os 
 import subprocess
-host = "0.0.0.0"
-host1 = '8.8.8.8'
-host2 = "8.8.8.4"
-host3 = "192.168.1.1"
-host4= "192.168.1.2"
-
-def pingCheck(host): #With Pin
-    response = os.system("ping -q " + host)
-    if response == 0:
-        print (host + " Reachable")
-    else:
-        print(host + " Unreachable")
-
-
 
 def pingCh(host):
     with open(os.devnull, "wb") as limbo:
@@ -24,7 +10,7 @@ def pingCh(host):
         else:
             print (host + " Unreachable")
 
-pingCh(host1)
-pingCh(host2)
-pingCh(host3)
-pingCh(host4)
+pingCh("8.8.8.8")
+pingCh("8.8.8.4")
+pingCh("192.168.1.1")
+pingCh("192.168.1.2")
