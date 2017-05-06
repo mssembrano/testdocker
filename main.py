c52477004bd0ae65d2d9d733736f954a534c7adf
Yatten <jonel.mawirat@bugomi.com>
2017-05-06 08:09:29 +0000

    Updated List

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-import os 
-import subprocess
-
-def pingCh(host):
-    with open(os.devnull, "wb") as limbo:
-        response = subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2", host],
-            stdout=limbo, stderr=limbo).wait()
-        if response==0:
-            print(host + " Reachable")
-        else:
-            print (host + " Unreachable")
-
-pingCh("8.8.8.8")
-pingCh("8.8.8.4")
-pingCh("192.168.1.1")
-pingCh("192.168.1.2")
-
-list1 = [1,2,3]
-
-for x in range(0,3):
-    print (list1[x])
+import pexpect
+child = pexpect.spawn("telnet 54.250.134.6 32773")
+child.sendline("\n\n")
+child.expect(">")
+child.sendline("enable")
+child.expect("#")
+child.sendline("config t")
+child.expect("#")
+child.sendline("hostname Router1")
+child.expect("#")
+child.sendline("end")
+child.expect("#")
     
